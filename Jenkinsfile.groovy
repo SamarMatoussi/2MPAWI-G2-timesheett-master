c74a7f0d04b9128de32f5dbd1803ec431ffd73c7
SamarMatoussi <148507480+SamarMatoussi@users.noreply.github.com>
2024-02-17 14:12:14 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,100 +1,14 @@
-  pipeline {
+pipeline {
     agent any
 
-    environment {
-    registry = "samarmatoussi/timesheet"
-    registryCredential = 'dockerhub'
-    dockerImage = 'samarmatoussi/timesheet:8'
-    emailRecipient = 'matoussi.samar20@gmail.com'
-    }
-
-    tools {
-    maven 'maven'
-    }
-
     stages {
-    stage('CHECKOUT GIT') {
-    steps {
-         checkout([$class: 'GitSCM', branches: [[name: '**/SamarMatoussi']], userRemoteConfigs: [[url: 'your_git_repository_url']]])
-    }
-    }
-    
-    stage('MVN CLEAN') {
-    steps {
-    script {
-    sh 'mvn clean'
-    }
-    }
-    }
-    
-    stage('ARTIFACT CONSTRUCTION') {
-    steps {
-    script {
-    sh 'mvn package'
-    }
-    }
-    }
-
-        stage('Sonar Tests') {
-            agent {
-                docker 'maven:3.8.3-openjdk-17'
-            }
+        
+        stage('Checkout') {
             steps {
                 script {
-                    unstash 'targetfiles'
-
-                    withSonarQube(installationName: 'sonar') {
-                        sh 'mvn sonar:sonar'
-                    }
+                    checkout([$class: 'GitSCM', branches: [[name: 'SamarMatoussi']], userRemoteConfigs: [[url: 'https://github.com/SamarMatoussi/2MPAWI-G2-timesheett-master.git/']]])
                 }
             }
         }
-
-
-        stage('UNIT TESTS') {
-    steps {
-    script {
-    echo 'launching Unit Tests...'
-    sh 'mvn test'
     }
-    }
-    }
-    
-    stage('PUBLISH TO NEXUS') {
-    steps {
-    script {
-    sh 'mvn deploy'
-    }
-    }
-    }
-    
-    stage('Building our image') {
-    steps {
-    script {
-    dir('/path/to/your/project') {
-    sh "docker build -t $dockerImage ."
-    }
-    }
-    }
-    }
-    
-    stage('Deploy our image') {
-    steps {
-    script {
-    docker.withRegistry('', registryCredential) {
-    dockerImage = docker.image(dockerImage)
-    dockerImage.push()
-    }
-    }
-    }
-    }
-    
-    stage('Send Email Notification') {
-    steps {
-  emailext subject: 'Jenkins Build Notification',
-                      body: 'The Jenkins pipeline has been completed successfully.',
-                      to: emailRecipient
-  }
-  }
-  }
-  }
+}
